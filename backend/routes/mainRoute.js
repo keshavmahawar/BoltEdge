18ea8ab3a5011ef699de9d04b6d4d6eeb4c830d3
kav310 <malimpatikavya@gmail.com>
2020-11-04 21:22:35 +0530

    competitors-list

--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+    registerUser,
+    loginUser,
+    competitors,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/competitors", competitors);
 
 module.exports = router;
