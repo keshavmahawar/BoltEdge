c7e2cf792ffbdef5ee1f47803d2624e828d80550
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-11-06 10:10:58 +0530

    Updated with required changes

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Button, Grid, TextField, Box, Checkbox, FormControlLabel } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
