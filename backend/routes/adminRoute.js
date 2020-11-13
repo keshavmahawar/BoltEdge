2bced6b13cd50cecb369277b89fa0aefd7f5d6e1
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-11-13 17:06:39 +0530

    Minor Changes (#53)

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -4,6 +4,4 @@ const adminController = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/login", adminController.loginAdmin);
-router.post("/signUp", adminController.registerAdmin);
-
 module.exports = router;
